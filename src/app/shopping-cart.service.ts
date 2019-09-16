459cabcdc5735beebd52438f1ef7d875c1da49e6
QauAli <muhammadali46000@gmail.com>
2019-09-16 14:31:51 +0500

    shopping-cart completed

--- src/app/shopping-cart.service.ts
+++ src/app/shopping-cart.service.ts
@@ -8,13 +8,12 @@ import { Item } from './models/item';
 @Injectable({
   providedIn: 'root'
 })
-export class ShoppingCartService implements OnInit{
+export class ShoppingCartService{
+
+  cartId;
 
   constructor(private db:AngularFireDatabase) {
    }
-
-   ngOnInit(): void {
-  }
 
   private create(){
    return this.db.list('/shopping-carts').push({
@@ -33,6 +32,7 @@ export class ShoppingCartService implements OnInit{
 
   private async CreateOrUpdateCart():Promise<string>{
     let cartId = localStorage.getItem('cartId');
+    this.cartId = cartId;
     if(!cartId){
       let cart = await this.create();
       localStorage.setItem('cartId',cart.key);
@@ -52,6 +52,10 @@ export class ShoppingCartService implements OnInit{
 
   async addToCart(product:Product){
     this.updateItemQuantity(product,1);//no await because we dont need result value otherwise you can use await also
+}
+
+clearCart(){
+  this.db.object('/shopping-carts/'+this.cartId).remove();
 }
 
   async updateItemQuantity(product:Product, change:number){
